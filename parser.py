c83edd9305280907277ddc1ed316a303b2dbed9c
beautifuleblan <dante.algery@gmail.com>
2023-09-21 09:12:09 +0300

    initial

--- parser.py
+++ parser.py
@@ -4,22 +4,23 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_site_content(proxy, proxy_type, domain):
+    proxy_formatted = {}
+
     if len(proxy) == 4:
-        proxy_formatted = {'http': f'{proxy_type.lower()}://{proxy[2]}:{proxy[3]}@{proxy[0]}:{proxy[1]}',
-                           'https': f'{proxy_type.lower()}://{proxy[2]}:{proxy[3]}@{proxy[0]}:{proxy[1]}'}
+        proxy_formatted['http'] = proxy_formatted['https'] = \
+            f'{proxy_type.lower()}://{proxy[2]}:{proxy[3]}@{proxy[0]}:{proxy[1]}'
     else:
-        proxy_formatted = {'http': f'{proxy_type.lower()}://{proxy[0]}:{proxy[1]}',
-                           'https': f'{proxy_type.lower()}://{proxy[0]}:{proxy[1]}'}
+        proxy_formatted['http'] = proxy_formatted['https'] = f'{proxy_type.lower()}://{proxy[0]}:{proxy[1]}'
 
     headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'
+                  ',application/signed-exchange;v=b3;q=0.7',
         'accept-encoding': 'gzip',
         'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
         'cache-control': 'no-cache',
         'pragma': 'no-cache',
         'sec-ch-ua': '"Chromium";v="110", "Not A(Brand";v="24", "Google Chrome";v="110"',
         'sec-ch-ua-mobile': '?0',
-        # 'sec-ch-ua-platform': 'macOS',
         'sec-fetch-dest': 'document',
         'sec-fetch-mode': 'navigate',
         'sec-fetch-site': 'none',
@@ -29,7 +30,7 @@ def get_site_content(proxy, proxy_type, domain):
         'referer': 'https://majestic.com/'
         }
 
-    formatted_domain = domain.replace('https://', '').replace('http://', '').replace('/', '')
+    formatted_domain = domain.split('//')[-1].split('/')[0]
     params = {'q': formatted_domain,
               'defaultQ': 'brightonseo.com'}
 
